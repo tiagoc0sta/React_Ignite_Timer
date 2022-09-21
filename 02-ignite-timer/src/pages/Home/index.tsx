74be99c34bcf938efa5392be999ccbb13acd6d44
Tiago Costa <tiagodpv@gmail.com>
2022-09-20 21:05:04 -0400

    add form

---
+++ 02-ignite-timer/src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+export function Home(){
+  return (
+    <div>
+      <form action="">
+        
+        <label htmlFor="">Vou trabalhar em</label>
+        
+      </form>      
+    </div>
+  )
+}
