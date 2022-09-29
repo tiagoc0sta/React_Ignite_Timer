ef1af6f4313d3cf17f43ad62e2d7677c484eb931
Tiago Costa <tiagodpv@gmail.com>
2022-09-29 12:45:05 -0400

    add home and history style

--- 02-ignite-timer/src/pages/Home/index.tsx
+++ 02-ignite-timer/src/pages/Home/index.tsx
@@ -9,14 +9,28 @@ export function Home(){
           <label htmlFor="">Vou trabalhar em</label>
           <TaskInput 
             id="task" 
+            list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
+
+
           />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1"/>
+            <option value="Projeto 2"/>
+            <option value="Projeto 3"/>
+            <option value="Outro"/>
+
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput 
             type="number" 
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutos.</span>
